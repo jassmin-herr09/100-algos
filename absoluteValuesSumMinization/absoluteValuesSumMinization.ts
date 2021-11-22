6340ce4f4a89c6e1659ded81dbbd73c5de6a22dc
jassmin-herr <jassminfernandez@gmail.com>
2021-11-21 22:00:40 -0600

    adds solutions 2 algos

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,14 @@
 export function absoluteValuesSumMinimization(a: number[]): number {
+    //get median value in a way, take left elem
+
+    const isEven = a.length % 2 === 0; 
+    return isEven ? a[a.length / 2 -1] : a[Math.floor(a.length / 2)]
+    
     return 5;
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+
+ console.log(absoluteValuesSumMinimization([2, 4, 7]));
+ console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+ console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+ console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
